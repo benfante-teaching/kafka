a496a8c6b4ff8112f728186e12d0788cf81409dd
Lucio Benfante <lucio@benfante.com>
2025-12-04 09:09:35 +0100

    Enhance HelloProducer: improve code formatting, add transaction support, and handle exceptions in sendInTransaction method

--- kafka-demo/src/main/java/com/benfante/teaching/kafka/clients/HelloProducer.java
+++ kafka-demo/src/main/java/com/benfante/teaching/kafka/clients/HelloProducer.java
@@ -7,6 +7,10 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.errors.AuthorizationException;
+import org.apache.kafka.common.errors.OutOfOrderSequenceException;
+import org.apache.kafka.common.errors.ProducerFencedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,22 +26,21 @@ public class HelloProducer {
         Properties props = getProps();
 
         try (Producer<String, String> producer = new KafkaProducer<>(props)) {
-            ProducerRecord<String, String> record =
-                    new ProducerRecord<>(topic, key, value);            
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
             producer.send(record);
             producer.flush();
         }
     }
 
-    public static void sendSync(String topic, String key, String value) throws InterruptedException, ExecutionException {
+    public static void sendSync(String topic, String key, String value)
+            throws InterruptedException, ExecutionException {
         Properties props = getProps();
 
         try (Producer<String, String> producer = new KafkaProducer<>(props)) {
-            ProducerRecord<String, String> record =
-                    new ProducerRecord<>(topic, key, value);
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
             RecordMetadata recordMetadata = producer.send(record).get();
-            log.info("Record sent to partition {} with offset {}",
-                    recordMetadata.partition(), recordMetadata.offset());
+            log.info("Record sent to partition {} with offset {}", recordMetadata.partition(),
+                    recordMetadata.offset());
             producer.flush();
         }
     }
@@ -54,17 +57,39 @@ public class HelloProducer {
         Properties props = getProps();
 
         try (Producer<String, String> producer = new KafkaProducer<>(props)) {
-            ProducerRecord<String, String> record =
-                    new ProducerRecord<>(topic, key, value);
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
             producer.send(record, (metadata, exception) -> {
                 if (exception != null) {
                     log.error("Error sending record", exception);
                 } else {
-                    log.info("Record sent to partition {} with offset {}",
-                            metadata.partition(), metadata.offset());
+                    log.info("Record sent to partition {} with offset {}", metadata.partition(),
+                            metadata.offset());
                 }
             });
             producer.flush();
+        }
+    }
+
+    public static void sendInTransaction(String topic, String key, String value) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("transactional.id", "my-transactional-id");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            producer.initTransactions();
+            producer.beginTransaction();
+            for (int i = 0; i < 100; i++)
+                producer.send(
+                        new ProducerRecord<>(topic, key, value + " " + Integer.toString(i)));
+            producer.commitTransaction();
+        } catch (ProducerFencedException | OutOfOrderSequenceException | AuthorizationException e) {
+            // We can't recover from these exceptions, so our only option is to close the producer
+            // and exit.
+            // producer.close();
+        } catch (KafkaException e) {
+            // For all other exceptions, just abort the transaction and try again.
+            // producer.abortTransaction();
         }
     }
 
