7e095e2e680e311b552bd9f9fabfb7e5736b70d3
Lucio Benfante <lucio@benfante.com>
2025-12-05 13:29:35 +0100

    Initialize Spring Boot Kafka project with Maven wrapper, configuration files, and basic application structure

---
+++ springbootkafka/src/test/java/com/benfante/teaching/springbootkafka/SpringbootkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benfante.teaching.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
